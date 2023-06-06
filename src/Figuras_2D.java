de901264d6e340af0857715574403e64c7460f62
Miguel-Paredes <os.miguelparedes@gmail.com>
2023-06-06 07:31:12 -0500

    Tercer Commit

--- src/Figuras_2D.java
+++ src/Figuras_2D.java
@@ -1,2 +1,16 @@
 public class Figuras_2D {
+    int nLados;//número de lados
+    //setea los atributos de  inicio
+    public Figuras_2D(int nLados) {
+        this.nLados = nLados;}
+    //retorna la variable
+    public int getnLados() {
+        return nLados;}
+    //setea la variable
+    public void setnLados(int nLados) {
+        this.nLados = nLados;}
+
+
+
+    //areaCuadrado(){};
 }
