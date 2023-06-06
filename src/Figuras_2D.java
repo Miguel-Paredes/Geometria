dc30b76a271b9b97d36217fd2529b1fb1d8946e8
Miguel-Paredes <os.miguelparedes@gmail.com>
2023-06-06 07:39:22 -0500

    Cuarto Commit

--- src/Figuras_2D.java
+++ src/Figuras_2D.java
@@ -1,16 +1,31 @@
 public class Figuras_2D {
     int nLados;//número de lados
+    float lado;//longitud de lado
     //setea los atributos de  inicio
+    public Figuras_2D(int nLados, float lado) {
+        this.nLados = nLados;
+        this.lado = lado;}
+
+    public Figuras_2D(float lado) {
+        this.lado = lado;}
+
     public Figuras_2D(int nLados) {
         this.nLados = nLados;}
+
     //retorna la variable
     public int getnLados() {
         return nLados;}
+
     //setea la variable
     public void setnLados(int nLados) {
         this.nLados = nLados;}
 
+    public float getLado() {
+        return lado;}
 
+    public void setLado(float lado) {
+        this.lado = lado;}
 
-    //areaCuadrado(){};
+    public float areaCuadrado(){};
+    private float perimetroCuadrado(){};
 }
